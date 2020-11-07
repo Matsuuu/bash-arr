da0b06a2265629da1ce6429386e7f1ba17433c61
Matsuuu <huhta.matias@gmail.com>
2020-11-07 12:16:34 +0200

    Tryin colors

--- bash-arr.sh
+++ bash-arr.sh
@@ -2,21 +2,26 @@
 
 PARAMS=()
 SELECTED=()
+POINTER=1
+SELECTED_COL='\033[32m;47m'
+OTHER_COL='\033[0m'
 # Set params
 for p in "$@";do
     PARAMS+=("$p")
 done
 
 print_params() {    
+    i=1
     for val in "${PARAMS[@]}"
     do
-        echo "[ ]" $val
+        if [ "$i" -eq "$POINTER" ]
+        then
+            echo -e ${SELECTED_COL}"[ ]" $val
+        else
+            echo -e ${OTHER_COL}"[ ]" $val
+        fi
+        i=$((i + 1))
     done
 }
-
-select opt in "${PARAMS[@]}"
-do
-    echo $opt
-done
 
 print_params
